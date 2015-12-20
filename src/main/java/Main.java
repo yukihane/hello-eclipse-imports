e6ce0446fc7a79f2e637b926079474640d9e27c4
yukihane <yukihane.feather@gmail.com>
2015-12-20 14:30:28 +0900

    class files

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import java.net.HttpURLConnection;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+
+import org.w3c.dom.DOMException;
+
+import a.ClassA;
+import javaj.ClassJavaj;
+
+public class Main {
+
+    public void exec() {
+        new ClassA();
+        new ArrayList<ClassJavaj>();
+        new DOMException((short) HttpURLConnection.HTTP_FORBIDDEN, null);
+        int i = JFrame.EXIT_ON_CLOSE;
+    }
+
+}
---
+++ src/main/java/javaj/ClassJavaj.java
@@ -0,0 +1,5 @@
+package javaj;
+
+public class ClassJavaj {
+
+}
